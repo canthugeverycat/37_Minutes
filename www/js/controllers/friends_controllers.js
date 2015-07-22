73059022b0f3e7cfc7a64c641b96434f884ba73f
canthugeverycat <dejanskorupan@gmail.com>
2015-07-22 16:50:36 +0200

    Added add/remove friend to/from group functionality, animated the friend cards

--- www/js/controllers/friends_controllers.js
+++ www/js/controllers/friends_controllers.js
@@ -1,6 +1,6 @@
 angular.module('friends.controllers',[])
 
-.controller('FriendsController',function($scope, $rootScope, InfoHandling, RESTFunctions) {
+.controller('FriendsController',function($scope, $rootScope, InfoHandling, RESTFunctions, $location) {
 
 	//Get a list of all of the user's friends
 	$scope.getFriendsList = function() {
@@ -38,6 +38,61 @@ angular.module('friends.controllers',[])
 				} else {
 					$rootScope.data.groups = response.Groups;
 					console.log(response.Groups);
+				}
+			}
+		});
+	};
+
+	//Get a specific group
+	$scope.getGroupItem = function(group, groupId) {
+		group !== null ? $rootScope.data.selectedGroup = group : selectedGroupId = groupId;
+		console.log('Entered group:');
+		console.log($rootScope.data.selectedGroup.groupId);
+		RESTFunctions.post({
+			url:'get-group-friends',
+			data:'Token=' + $rootScope.login.token + '&groupId=' + (group !== null ? $rootScope.data.selectedGroup.groupId : selectedGroupId),
+			callback: function(response) {
+				console.log('groupid ' + $rootScope.data.selectedGroup.groupId);
+				if (response.error) {
+					InfoHandling.set('getGroupItemFailed',response.error.errorMessage, 2000);
+				} else {
+					$rootScope.data.selectedGroupFriends = response.Friends;
+				}
+			}
+		});
+		$location.path('/editGroup');
+	};
+
+	//Add a friend to a group
+	$rootScope.addFriendToGroup = function(groupId, friendMail, friendId) {
+		RESTFunctions.post({
+			url:'add-in-group',
+			data:'Token=' + $rootScope.login.token + '&groupId=' + groupId + (friendMail !== null ? '&friendsMail=' + friendmail : '') + (friendId !== null ? '&friendsUid=' + friendId : ''),
+			callback: function(response) {
+				if (response.error) {
+					InfoHandling.set('addFriendToGroupFailed', response.error.errorMessage, 2000);
+				} else {
+					console.log('addFriendToGroup:');
+					console.log(response);
+					InfoHandling.set('addFriendToGroupSuccessful',response.Message, 2000, 'bg-energized');
+					$scope.getGroupItem(null, groupId);
+				}
+			}
+		});
+	};
+
+	//Remove friend from a group
+	$rootScope.removeFriendFromGroup = function(groupId, friendId) {
+		RESTFunctions.post({
+			url:'remove-from-group',
+			data:'Token=' + $rootScope.login.token + '&groupId=' + groupId + '&friendId=' + friendId,
+			callback: function(response) {
+				if (response.error){
+					InfoHandling.set('removeFriendFromGroup',response.error.errorMessage, 2000);
+				} else {
+					console.log('getGroupItem(' + groupId + ')');
+					InfoHandling.set('removeFriendFromGroupSuccessful',response.Message, 2000, 'bg-energized');
+					$scope.getGroupItem(null, groupId);
 				}
 			}
 		});
